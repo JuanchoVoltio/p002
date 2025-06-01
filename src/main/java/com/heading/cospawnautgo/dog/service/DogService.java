2856fb684d2da36e4175ad0a9768667e775a8640
Henry L. Gómez Ortiz <henryl.gomez@gmail.com>
2025-06-01 16:23:03 -0500

    Add create feature to Dog service

--- src/main/java/com/heading/cospawnautgo/dog/service/DogService.java
+++ src/main/java/com/heading/cospawnautgo/dog/service/DogService.java
@@ -1,16 +1,29 @@
 package com.heading.cospawnautgo.dog.service;
 
 import com.heading.cospawnautgo.dog.model.Dog;
+import com.heading.cospawnautgo.dog.repository.DogRepository;
 import com.heading.cospawnautgo.global.service.BasicCrudService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class DogService implements BasicCrudService<Dog, String> {
+
+    protected DogRepository repository;
+
+    @Autowired
+    public DogService(DogRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Dog create(Dog entity) {
-        return null;
+        entity.setId(entity.getOwner().getId() + entity.getName());
+        entity.setRegisteredOn(LocalDateTime.now());
+        return repository.save(entity);
     }
 
     @Override
